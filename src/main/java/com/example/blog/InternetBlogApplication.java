921fd0fd5536aec3632e283f243300227c2a886c
Andruschenko Vitaliy <vandriev@bk.ru>
2023-08-23 12:54:22 +0300

    Initial commit

---
+++ src/main/java/com/example/blog/InternetBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetBlogApplication.class, args);
+	}
+
+}
